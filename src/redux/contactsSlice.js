9808c9fd718cb255fb4d5d5b1a1831e8b6781db6
KonstantinYev <109310447+KonstantinYevchuk@users.noreply.github.com>
2023-03-30 21:52:20 +0300

    prietter

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,29 +1,31 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from '@reduxjs/toolkit';
 // import { persistReducer } from 'redux-persist';
 // import storage from 'redux-persist/lib/storage';
 
 // const contactsInitialState = [];
-const STORAGE = "contact";
+const STORAGE = 'contact';
 const getSavedContacts = () => {
   const savedContacts = localStorage.getItem(STORAGE);
-    if(savedContacts !== null) {
-      const parsedContacts = JSON.parse(savedContacts)
-      return parsedContacts
-    }
-    return []
-}
+  if (savedContacts !== null) {
+    const parsedContacts = JSON.parse(savedContacts);
+    return parsedContacts;
+  }
+  return [];
+};
 const constacsSlice = createSlice({
-  name: "contacts",
+  name: 'contacts',
   initialState: getSavedContacts,
   reducers: {
     addContact: {
       reducer(state, action) {
         state.some(contact => contact.name === action.payload.name)
-        ? alert(`${action.payload.name}, Contact with such name is already exists!`)
-        : state.push(action.payload);
-        localStorage.setItem(STORAGE, JSON.stringify(state))
+          ? alert(
+              `${action.payload.name}, Contact with such name is already exists!`
+            )
+          : state.push(action.payload);
+        localStorage.setItem(STORAGE, JSON.stringify(state));
       },
-      prepare({name, number}) {
+      prepare({ name, number }) {
         return {
           payload: {
             name,
@@ -34,17 +36,14 @@ const constacsSlice = createSlice({
       },
     },
     deleteContact(state, action) {
-        return state.filter(contact => contact.id !== action.payload)
-           
-        
-        // const index = state.findIndex(contact => contact.id === action.payload);
-        // state.splice(index, 1);  
+      return state.filter(contact => contact.id !== action.payload);
+
+      // const index = state.findIndex(contact => contact.id === action.payload);
+      // state.splice(index, 1);
     },
- 
   },
 });
 
-
-export const { addContact, deleteContact, filterContact } = constacsSlice.actions;
+export const { addContact, deleteContact, filterContact } =
+  constacsSlice.actions;
 export const contactsReducer = constacsSlice.reducer;
-
